c007b1fceb42ef81e6a1a87616c01ace9caa8647
Jordan Boekel <jfb98is@live.com.au>
2021-03-03 16:10:22 +1100

    updates to PT1.1

--- PassTask1.1/PassTask1.1/Program.cs
+++ PassTask1.1/PassTask1.1/Program.cs
@@ -11,7 +11,6 @@ namespace PassTask1._1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
 
             Message myMessage;
             myMessage = new Message("Hello, world!");
