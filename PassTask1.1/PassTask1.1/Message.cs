31ce2190a69f896a439db9c8a7e5ae1bf6d7a2a7
Jordan Boekel <jfb98is@live.com.au>
2021-03-03 12:58:50 +1100

    Adding first task to git

---
+++ PassTask1.1/PassTask1.1/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassTask1._1
+{
+    class Message
+    {
+        private string _text;
+        public Message(string txt) //Constructor, not a method
+        {
+            _text = txt;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_text);
+        }
+    }
+}
---
+++ PassTask1.1/PassTask1.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassTask1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            Message myMessage;
+            myMessage = new Message("Hello, world!");
+
+            Message[] messages = new Message[4];
+
+            messages[0] = new Message("Why hello, newby");
+            messages[1] = new Message("You're awesome!");
+            messages[2] = new Message("Overrated...");
+            messages[3] = new Message("Classic :)");
+
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            if(name == "Jordan")
+            {
+                messages[0].Print();
+            }
+            else if(name == "Kim")
+            {
+                messages[1].Print();
+            }
+            else if(name == "Keanu")
+            {
+                messages[2].Print();
+            }
+            else
+            {
+                messages[3].Print();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
