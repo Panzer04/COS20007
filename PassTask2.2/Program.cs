b8a55bae36c13a23b10fd3ed438b62036e01b662
JORDAN BOEKEL <jfb98is@live.com.au>
2021-03-20 14:14:14 +1100

    Adding project 2.2.

---
+++ PassTask2.2/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using SplashKitSDK;
+
+
+namespace ShapeDrawer
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            new Window("Shape Drawer", 800, 600);
+            Shape myShape = new Shape();
+            do
+            {
+                SplashKit.ProcessEvents(); //Check user inputs - should only call once
+                SplashKit.ClearScreen(); //Clear screen to white                
+                if (SplashKit.MouseClicked(MouseButton.LeftButton))
+                {
+                    myShape.X = SplashKit.MouseX();
+                    myShape.Y = SplashKit.MouseY();
+                }
+                if (myShape.IsAt(SplashKit.MousePosition()) && SplashKit.KeyTyped(KeyCode.SpaceKey)) 
+                {
+                    myShape.Color = SplashKit.RandomRGBColor(255);
+                }
+                myShape.Draw();
+                SplashKit.RefreshScreen(); //Target FPS? Should have an uint argument?
+            } while (!SplashKit.WindowCloseRequested("Shape Drawer"));
+        }
+    }
+}
---
+++ PassTask2.2/Shape.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    class Shape
+    {
+        private Color _color;
+        private float _x, _y;
+        private int _width, _height;
+        public Shape() //Class constructor
+        {
+            _color = Color.Green;
+            _x = 0; 
+            _y = 0;
+            _width = 100;
+            _height = 100;
+        }
+
+
+        public Color Color
+        {
+            get
+            {
+                return _color;
+            }
+
+            set
+            {
+                _color = value;
+            }
+        }
+        public float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public float Y
+        {
+            get
+            {
+                return _y; 
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = value;
+            }
+        }
+
+        public void Draw()
+        {
+            SplashKit.FillRectangle(_color, _x, _y, _width, _height);
+        }
+
+        public bool IsAt(Point2D pt)
+        {
+            if((pt.X > _x) && (pt.X < (_x + _width)))
+            {
+                if ((pt.Y > _y) && (pt.Y < (_y + _height)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
