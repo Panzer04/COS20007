12e45278360fc15a53b14301a5c83e0fac54b577
JORDAN BOEKEL <jfb98is@live.com.au>
2021-03-22 12:16:01 +1100

    added task 3.1 (copy of 2.2 with extra additions)

---
+++ PassTask3.1/Drawing.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    class Drawing
+    {
+        
+        private Color _background;
+        private readonly List<Shape> _shapes;
+        public Drawing(Color background) //Constructor
+        {
+            _background = background;
+            _shapes = new List<Shape>();
+        }
+        public Drawing() : this(Color.White)
+        {
+
+        }
+
+
+        public List<Shape> SelectedShapes
+        {
+            get
+            {
+                List<Shape> result = new List<Shape>();
+                foreach(Shape s in _shapes)
+                {
+                    if (s.Selected)
+                    {
+                        result.Add(s);
+                    }
+                }
+                return result;
+            }
+        }
+
+        public int ShapeCount
+        {
+            get
+            {
+                return _shapes.Count;
+            }
+        }
+
+        public Color Background
+        {
+            get
+            {
+                return _background;
+            }
+            set
+            {
+                _background = value;
+            }
+        }
+
+        public void Draw()
+        {
+            SplashKit.ClearScreen(_background);
+            foreach(Shape s in _shapes)
+            {
+                s.Draw();
+            }
+        }
+
+        public void SelectSchapesAt(Point2D pt)
+        {
+            foreach(Shape s in _shapes)
+            {
+                if (!s.Selected) //Don't "unselect" shapes
+                {
+                    s.Selected = s.IsAt(pt);
+                }
+            }
+        }
+
+        public void AddShape(Shape s)
+        {
+            _shapes.Add(s);
+        }
+
+        public void RemoveShape(Shape s)
+        {
+            _shapes.Remove(s);
+        }
+        
+    }
+}
